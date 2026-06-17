c76fe0fdc979c383c51501fcc271dcb6dc52f3ae
geancavalcante <geanfeitosacavalcanteof@gmail.com>
2026-06-17 16:13:02 -0300

    Aprendendo sobre arrays

--- index.php
+++ index.php
@@ -11,10 +11,42 @@
 
         $receitas = [
 
-            
-            "Pamonha",
-            "Limonada",
-            "Macarronada",
+            "limonada" => [
+
+                "nome da receita" => "limonada",
+
+                "ingredientes" => [
+                    "2 Limões", 
+                    "300 ML de Água",
+                    "3 colheres de açucar"
+                ],
+
+            ],
+
+            "macarronda" => [
+
+                "nome da receita" => "macarronda",
+
+
+                "ingredientes" => [
+
+                    "1 litro de água", 
+                    "Sal a gosto", 
+                    "1 pouco de óleo",
+                    "1 pouco de óleo",
+                    "Coentro",
+                    "1 lata de milho verde e ervilha",
+                    "Queijo ralado",
+                    "1 pacote de macarrão parafuso",
+                    "Frango cozido desfiado",
+                    "1 colher de margarina",
+                    "2 tomates maduros",
+                    "1 lata de extrato de tomate",
+                    "Queijo",
+                    "Presunto",
+ 
+                ]
+            ],          
         ]; 
     ?>
 
@@ -29,26 +61,23 @@
     <hr>
 
     <main>
-        <ul>      
-            <?php 
-                  foreach ($receitas as $receita){
-                    if ($receita == "Limonada"){
-            ?>
-                <li>
-                    <h2><?=$receita?></h2>
-                </li>
+        <ul>     
+            <?php foreach($receitas as $receita): ?> 
+               <div>
 
-            <?php 
-            }
-                elseif ($receita == "Macarronada"){  
-            ?>
-                <li>
-                    <h2><?=$receita?></h2>
-                </li>
-            <?php 
-            }
-            };         
-               ?>
+                    <h2><?=$receita["nome da receita"]?></h2>
+
+                    <?php foreach($receita["ingredientes"] as $ingredientes):?>
+                      
+                        <li><?=$ingredientes?></li>
+
+
+                    <?php endforeach;?>
+                    
+               </div>
+
+            <?php endforeach;?>
+            
                  
         </ul>
     </main>
