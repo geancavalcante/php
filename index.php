0891c6365a7cc31e15274e0f7e88e389cb8c0b48
geancavalcante <geanfeitosacavalcanteof@gmail.com>
2026-06-17 16:48:45 -0300

    Aprendendo sobre funções

--- index.php
+++ index.php
@@ -3,91 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de Receitas</title>
+    <title>Lista de Usuários</title>
 </head>
 <body>
 
     <?php 
 
-        $receitas = [
+        $usuarios = [
 
-            "limonada" => [
-
-                "nome da receita" => "limonada",
-
-                "ingredientes" => [
-                    "2 Limões", 
-                    "300 ML de Água",
-                    "3 colheres de açucar"
-                ],
-
+            "Gean" => [
+                "ativo" => true,
             ],
 
-            "macarronda" => [
+            "Usuário 2" => [
 
-                "nome da receita" => "macarronda",
+                "ativo" => false,
+            ],
 
+            "Usuário 3" => [
 
-                "ingredientes" => [
+                "ativo" => true,
+            ],
 
-                    "1 litro de água", 
-                    "Sal a gosto", 
-                    "1 pouco de óleo",
-                    "1 pouco de óleo",
-                    "Coentro",
-                    "1 lata de milho verde e ervilha",
-                    "Queijo ralado",
-                    "1 pacote de macarrão parafuso",
-                    "Frango cozido desfiado",
-                    "1 colher de margarina",
-                    "2 tomates maduros",
-                    "1 lata de extrato de tomate",
-                    "Queijo",
-                    "Presunto",
- 
-                ]
-            ],          
-        ]; 
-    ?>
-
-    <header>
-        <h1>
-            <?php 
-                echo "Receitas"        
-            ?>
-        </h1>
-    </header>
-
-    <hr>
-
-    <main>
-        <ul>     
-            <?php foreach($receitas as $receita): ?> 
-               <div>
-
-                    <h2><?=$receita["nome da receita"]?></h2>
-
-                    <?php foreach($receita["ingredientes"] as $ingredientes):?>
-                      
-                        <li><?=$ingredientes?></li>
+        ];
 
 
-                    <?php endforeach;?>
-                    
-               </div>
+        function ListarUsuarios ($usuarios){
 
-            <?php endforeach;?>
-            
-                 
-        </ul>
-    </main>
+            foreach ($usuario as $usuarios){
+                if ($usuario["ativo"]){     
+        ?>       
+                    <i>Usuário ativo ✅</i>
 
-    <hr>
+        <?php 
+            } else{
+        ?>
+                    <i>Usuário Inativo ❌</i>
 
-    <footer>
-
-    </footer>
+        <?php  
+                }   
+            }
+        } 
+        ?>
     
+    
+
     
     
 </body>
